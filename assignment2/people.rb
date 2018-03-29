944c3997be711477c05782cb679e9c2acd1eef94
gusmattos1 <gusmattos@gmail.com>
2018-03-29 15:09:59 -0400

    exercise2 done

---
+++ assignment2/people.rb
@@ -0,0 +1,55 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello, my name is #{@name}"
+  end
+
+end
+#----------------------------------------------------------------------------
+class Student < Person
+
+  def initialize(name)
+    super (name)
+  end
+
+  def learn
+    puts "I get it!"
+  end
+
+end
+#----------------------------------------------------------------------------
+class Instructor < Person
+
+  def initialize(name)
+    super (name)
+  end
+
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
+
+end
+
+#----------------------------------------------------------------------------
+
+new_student = Student.new("Chris")
+
+puts new_student.inspect
+new_student.greet
+new_student.learn
+
+puts "\n\n"#----------------------------------------------------------------------------
+
+
+new_instructor = Instructor.new("Nadia")
+
+puts new_instructor.inspect
+new_instructor.greet
+new_instructor.teach
+
+new_instructor.learn #------ERROR -------
+#THE CLASS Instructor do not know  the method learn!!!
